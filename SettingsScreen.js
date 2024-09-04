978c4ae61ed958238ee7174a99b10837d431fba5
cj03nes <72360646+cj03nes@users.noreply.github.com>
2024-09-04 16:23:50 -0500

    Update SettingsScreen.js

--- SettingsScreen.js
+++ SettingsScreen.js
@@ -1,6 +1,7 @@
 // SettingsScreen.js
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
+import accountSettings from './util.sol';
 
 export default function SettingsScreen() {
   return (
