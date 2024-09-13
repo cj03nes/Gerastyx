f3799bb4c9d58b2cd40d93c5b06a09d738ad295b
cj03nes <72360646+cj03nes@users.noreply.github.com>
2024-09-13 12:08:34 -0500

    Update Zeropoint.sol

--- contracts/Zeropoint.sol
+++ contracts/Zeropoint.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/ERC20.sol";
+import "@openzeppelin/ERC20Burnable.sol";
+import "@openzeppelin/Ownable.sol";
 
 contract Zeropoint is ERC20, ERC20Burnable, Ownable {
     constructor(address initialOwner)
