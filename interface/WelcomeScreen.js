2f9d6d0b4c1182186f39f62ce9621d945ba3b4a8
cj03nes <72360646+cj03nes@users.noreply.github.com>
2024-09-13 15:05:53 -0500

    Update WelcomeScreen.js

--- interface/WelcomeScreen.js
+++ interface/WelcomeScreen.js
@@ -1,1 +1,17 @@
 
+    <View style={styles.container}>
+         <header style="background-color: #4B006E; color: #FFD700;">
+      <Text style={styles.title}>Welcome to Gerastyx Industries</Text>
+      
+      
+      
+     
+      <Button title="SignUp" onPress={() =>  SignUp </Button>
+    
+    
+    
+      <Button title="Login" onPress={() => Login </Button>
+      
+     
+      
+</Header>
