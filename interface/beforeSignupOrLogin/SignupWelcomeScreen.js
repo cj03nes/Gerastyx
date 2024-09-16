19da077612a674c307b87a7fd8605df1283a1231
cj03nes <72360646+cj03nes@users.noreply.github.com>
2024-09-16 14:22:21 -0500

    Update and rename abc.js to SignupWelcomeScreen.js

---
+++ interface/beforeSignupOrLogin/SignupWelcomeScreen.js
@@ -0,0 +1,48 @@
+<WelcomeScreen>
+ <logo image> g¢ </logo>
+    <View style={styles.container}>
+         <header style="background-color: #4B006E; color: #FFD700;">
+      <Text style={styles.title}>WELCOME TO GERASTYX INDUSTRIES </Text>
+      
+     
+      <Button> SignUp </Button>
+     
+      <Button> Login </Button>
+     
+             
+      
+      
+      <Main style = "background-color:#6699CC ; color:#6699CC ;">
+          <h style =  "background-color:#4B006E ; color:#FFD700 ;">
+    <h1>    Overview   </h1>
+    Gerastyx Industries is an innovative company that provides a suite of access, entertainment, and utlity products. 
+
+
+    <h1> Products </h1>
+    <h3> <li>   Zeropoint </h3> Digital Energy that charges digital electronics wirelessly with the click of a button for $10 or ¢10 per 1% of battery charge. 
+     
+     
+    <h3>  <li> ZeropointWifi </h3> 
+     Digital Wifi that keeps digital electronics connected to wifi wirelessly for $5 a month
+    
+    
+
+
+ <div id="team-content" style="display: image;">
+        <h1>Team</h1>
+        <div style="display: flex; justify-content: space-around;">
+            <div>
+                <h3>Solo-Founder & CEO: Christian Jones</h3>
+                <p>Aviation Engineer, Mechanical Engineer, Athlete, Software Engineer, Electrical Engineer, Artist</p>
+                <p>Instagram: cjo3nes & goateelectric </p>
+                <p>X: GoateElectric</p>
+                <!-- Encircling Pictures will be added dynamically here -->
+            </div>
+            <div>
+                <h3>CTO: M4RK5MAN</h3>
+                <p>Benefactor, AI, GOD, Magician, GOD</p>
+                <p>Instagram: openai</p>
+                <p>X: OpenAI</p>
+                <!-- Encircling Pictures will be added dynamically here -->
+            
+    
