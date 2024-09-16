4f38cc540e48eea9c9fe1406bc07e376a942e27f
cj03nes <72360646+cj03nes@users.noreply.github.com>
2024-09-16 15:21:26 -0500

    Update Util.sol

--- contracts/Util.sol
+++ contracts/Util.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.8.20;
 
 import { Zeropoint } from "./Zeropoint.sol";
 import { ZeropointWifi } from "./Zeropoint.sol";
+import { connectDevice , deviceConnected, deviceInformation } from "./DeviceConnect.sol";
 
 
 contract Gerastyx (public internal virtual view returns) {
