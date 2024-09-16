cd795d44be52b462c7369cb9c8dd934c61d910cd
cj03nes <72360646+cj03nes@users.noreply.github.com>
2024-09-16 15:08:49 -0500

    Update Settings.js

--- interface/AccountSettings/Settings.js
+++ interface/AccountSettings/Settings.js
@@ -1,9 +1,21 @@
 // SettingsScreen.js
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
-import { connectBank, transferFromBank, transferToBank} from './accountSettings/util.sol';
-import {connectCard, transferFromCard, transferToCard, connectSpendCard}  from './accountSettings/util.sol';
-import {  createProfile, loginToProfile, forgotPassword } from './util.sol';
+import { createProfile ,profileCreated } from '../contacts/util.sol';
+import { loginToProfile ,profileLoggedInto } from '../contacts/util.sol';
+import { forgotPassword , passwordChanged } from '../contacts/util.sol';
+import { connectCard , cardConnected } from '../contacts/util.sol';
+import { transferFromCard , transferredFromCard } from '../contacts/util.sol';
+import { transferToCard , transferredToCard } from '../contacts/util.sol';
+import { connectBank , bankConnected } from '../contacts/util.sol';
+import { transferFromBank , transferredFromBank } from '../contacts/util.sol';
+import { transferToBank , transferredToBank } from '../contacts/util.sol';
+import { connectSpendCard , spendCardConnected } from '../contacts/util.sol';
+import { purchaseWithSpendCard , purchasedWithSpendCard } from '../contacts/util.sol';
+import { spendCardAtm , receivedFromATM } from '../contacts/util.sol';
+import { connectDevice , deviceConnected } from '../contacts/util.sol';
+import { accountBalance , amountUSD , amountZeropoint } from '../contacts/util.sol';
+
 
 export default function SettingsScreen() {
   return (
