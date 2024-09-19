1efb2c6a32551eb711285084a1e05eeacffc85d3
cj03nes <72360646+cj03nes@users.noreply.github.com>
2024-09-19 13:30:51 -0500

    Update Header.js

--- interface/Header.js
+++ interface/Header.js
@@ -3,3 +3,36 @@
 // Navigation => top far right
 //Login => next to navbar
 // Account Info => next to login only if logged in
+
+
+
+
+<Header style = "
+
+background-color: #4B006E; 
+color: #FFD700;">
+    
+   <img src= "    "/>{ }
+    
+      <text style="
+      margin-left: 250px;"> <b> GERASTYX INDUSTRIES</b>  </text>
+   
+   
+   <button style="
+margin-right: -500px;
+background-color: #000000; 
+color: #FFD700;
+title: signUpOrLogin" >  <b> SignUp / Login </b> </button> 
+   
+   <button style="
+margin-left: 540px;
+background-color: #000000; 
+color: #FFD700; "> <b> Connect Device</b> </button>
+   
+   <button style="
+margin-left: 750px;
+margin-top: -20px;
+background-color: #000000; 
+color: #FFD700; "> <b> Settings </b>  </button>
+    
+</Header>
