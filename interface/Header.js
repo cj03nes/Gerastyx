9ba1a4e3b64799faa2212b6d786bd62a78feeaab
cj03nes <72360646+cj03nes@users.noreply.github.com>
2024-09-19 13:46:57 -0500

    Update Header.js

--- interface/Header.js
+++ interface/Header.js
@@ -23,7 +23,7 @@ import { accountSettings } from "../comtracts/Util.sol";
 background-color: #4B006E; 
 color: #FFD700;">
     
-   <img src= " '../images/GerastyxLogo.png'    "/>{ }
+   <img src= "../images/GerastyxLogo.png"/>{ }
     
       <text style="
       margin-left: 250px;"> <b> GERASTYX INDUSTRIES</b>  </text>
@@ -38,12 +38,14 @@ title: signUpOrLogin" >  <b> SignUp / Login </b> </button>
    <button style="
 margin-left: 540px;
 background-color: #000000; 
-color: #FFD700; "> <b> Connect Device</b> </button>
+color: #FFD700;
+title: connectDevice"> <b> Connect Device</b> </button>
    
    <button style="
 margin-left: 750px;
 margin-top: -20px;
 background-color: #000000; 
-color: #FFD700; "> <b> Settings </b>  </button>
+color: #FFD700;
+title:accountSettings"> <b> Settings </b>  </button>
     
 </Header>
