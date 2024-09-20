0e0f00e73b2e024da5b7273745ae7cc242da17f6
cj03nes <72360646+cj03nes@users.noreply.github.com>
2024-09-20 11:00:38 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,37 @@
+<Header style = "
 
-const header = `
-    <header style="background-color: #4B006E; color: #FFD700;">
-        <div id="balances">
-            <p>USD Balance: $0.00</p>
-            <p>Zeropoint Balance: 0 ZP</p>
+background-color: #4B006E; 
+color: #FFD700;">
+    
+   <img src= "../images/GerastyxLogo.png"/>{ }
+    
+      <text style="
+      margin-left: 250px;"> <b> GERASTYX INDUSTRIES</b>  </text>
+   
+   
+   <button style="
+margin-right: -500px;
+background-color: #000000; 
+color: #FFD700;
+title: signUpOrLogin" >  <b> SignUp / Login </b> </button> 
+   
+   <button style="
+margin-left: 540px;
+background-color: #000000; 
+color: #FFD700;
+title: connectDevice"> <b> Connect Device</b> </button>
+   
+   <button style="
+margin-left: 750px;
+margin-top: -20px;
+background-color: #000000; 
+color: #FFD700;
+title:accountSettings"> <b> Settings </b>  </button>
+    
+     <div id="balances">
+<p>  <b> USD Balance: $0.00 </b> </p>
+<p> <b>  Zeropoint Balance: 0 ZP </b> <br/>
+<i> ZeropointInUSD: $0.00 </i></p>
         </div>
         
-    </header>
+</Header>
