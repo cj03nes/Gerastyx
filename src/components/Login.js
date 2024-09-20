764bf13458f3bc5f704a333f8ed427d535987941
cj03nes <72360646+cj03nes@users.noreply.github.com>
2024-09-20 11:26:17 -0500

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,8 +2,38 @@ import styled from "styled-components";
 
 
 const Login = (props) => {
-  return ( <div> Login </div> );
+  return ( 
     
+    <Container>
+    <Content>
+    <div> Login </div> );
+    </Content>
+    </Container>
 };
 
+const Container= styled.section"
+overflow: hidden;
+display: flex;
+flex-direction: column;
+text-align: center;
+height: 100vh;";
+
+ const Content = styled.div"
+ margin-bottom: 10vw;
+width: 100l;
+position: relative;
+min-height: 100vh;
+box-sizing: border-box;
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+padding: 80px 40px;
+height: 100l;
+";
+
+
 export default Login;
+
+
+
