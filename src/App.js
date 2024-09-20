8ef651318235439b450fcfeda1dd5a4581e07d42
cj03nes <72360646+cj03nes@users.noreply.github.com>
2024-09-20 14:29:58 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Switch, Route} from "react-router";
+import { Header } from "./components/Header.js";
 import { Login } from "./components/Login.js";
 import "./App.css";
 
 function App() {
   return(
     <div className= "App"> 
-    <Router> 
+    <Router>
+    <Header/>
     <Switch> 
     <Route exact path= "/">    <Login/>      </Route>
     </Switch>
