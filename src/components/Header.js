d18b7a051e0747c38e4a6a06402885246e9f029a
cj03nes <72360646+cj03nes@users.noreply.github.com>
2024-09-20 10:48:25 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,9 @@
 
+const header = `
+    <header style="background-color: #4B006E; color: #FFD700;">
+        <div id="balances">
+            <p>USD Balance: $0.00</p>
+            <p>Zeropoint Balance: 0 ZP</p>
+        </div>
+        
+    </header>
