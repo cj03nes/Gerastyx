3eb1015174365a0ae303c9b118a805647dae5f00
cj03nes <72360646+cj03nes@users.noreply.github.com>
2024-09-20 11:11:43 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
+import { BrowserRouter as Router, Switch, Route} from "react-router";
+import "./App.css";
 
+function App() {
+  return(
+    <div className= "App"> 
+    <Router> 
+    <Switch> 
+    <Route> </Route>
+    </Switch>
+    </Router>
+    </div>
+  );
+}
+
+export default App;
