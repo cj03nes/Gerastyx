a42b91902ff63ea321608b17a73a07e094c67fd6
cj03nes <72360646+cj03nes@users.noreply.github.com>
2024-12-02 11:18:44 -0600

    Update PioneerNFT.sol

--- contracts/PioneerNFT.sol
+++ contracts/PioneerNFT.sol
@@ -1,3 +1,56 @@
 // buy
 // sell / redeem
-// trade / auction / list
+// utility
+
+
+
+contract PioneerNFT ={
+
+// * image or images here *
+// * quantity here *
+// * choose / change original price here
+
+function buy ={
+// user buys NFT
+// owner gets 20% from NFT
+// NFT contract holds the 80% until sold
+
+PioneerNFT price = 5 Pi;
+if msg.sender[balance] < 5 Pi then return error, " Insufficient Funds",
+else if msg.sender[balance] >= 5 Pi then return true,
+msg.sender[balance] - 5 Pi = msg.sender[newBalance];
+5 Pi - 20% owner[balance] = 4 Pi;
+PioneerNFTContract[balance] + 4 Pi = PioneerNFTContract[newBalance];     }
+
+
+
+
+function sell ={
+// get PioneerNFT price, not immutable
+// user sells NFT back to Contract
+// user gets money back from conntract
+
+PioneerNFT price = numberOf PioneerNFT % PioneerNFTContract[balance];
+     (require == msg.sender[PioneerNFT.balances]);
+
+if msg.sender[PioneerNFT.balances] < 1 then return error, " Insufficient Funds",
+else if msg.sender[PioneerNFT.balances > 1 then return true,
+msg.sender[balance] - PioneerNFT = msg.sender[newBalance];
+PioneerNFT + PioneerNFTContract[balance];
+msg.sender[balance] + PioneerNFT price = msg.sender[newBalance];
+PioneerNFTContract[balance] - PioneerNFT price = PioneerNFTContract[newBalance];
+}
+
+
+
+function utility ={
+     (require == msg.sender[PioneerNFT.balance]);
+if msg.sender[PioneerNFT.balance < 1 then return error, " Insufficient NFT Balance",
+else if msg.sender[PioneerNFT.balance] >= 1 then return true,
+// * Enter PioneerNFT rewards here { miningRate, stakingBoost, no trxFees, % from trxFees, etc *    }
+
+
+
+
+
+}
