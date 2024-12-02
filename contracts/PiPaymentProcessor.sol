523fc0aac269b0ab5abf1b76d31d2123c0d1feec
cj03nes <72360646+cj03nes@users.noreply.github.com>
2024-12-02 10:46:49 -0600

    Update PiPaymentProcessor.sol

--- contracts/PiPaymentProcessor.sol
+++ contracts/PiPaymentProcessor.sol
@@ -1,3 +1,20 @@
+pragma solidity ^0.8;
+
 // pay in x 
 // get USD denomination
 // accept x in $PI [USD denomination]
+
+import "../InstilledInteroperability" ;
+import Pi;
+
+
+
+contract PiPaymentProcessor ={
+   (require == verifiedChain);
+   (require == verifiedTokenAsset); 
+
+
+
+
+
+}
