d8682395bbc8a591a52927b27fdbb30d6d8e887d
cj03nes <72360646+cj03nes@users.noreply.github.com>
2024-12-02 13:00:31 -0600

    Update InsuranceAggregator.sol

--- contracts/InsuranceAggregator.sol
+++ contracts/InsuranceAggregator.sol
@@ -1,7 +1,40 @@
+pragma solidity ^0.8;
+
+// import kyc/aml info too
+
 // enter age, sex, and citizenship info
+function vagueWho ={
+age = $age;
+gender = male || female;
+citizenship = $citizenship;
+zip code = $zipCode;
+emplyment status = working || not working;
+return vagueWho[information];}
 
 // choose type of insurance
+type of insurance needed = home || auto || life || health || motorcycle || bundle ;
 
-// find the best rates
+// find the best rates from an aggregation of providers
+function insuranceAggregation = {
+     (require == vagueWho[information]) ;
+[1. https://www.progressive.com/ ],
+[2. https://www.geico.com/],
+[3. https://www.statefarm.com/],
+[4. https://www.allstate.com/],
+[5. https://www.libertymutual.com/],
+[6. https://www.nationwide.com/]   ;
+
+if geico price < progressive price return geico;
+if statefarm price > allstate price return allstate;
+if allstate price > progressive price return progressive;
+
+}
+
 
 // users pay in Pi => insurance agency accepts USD
+function payPiToPayUSD ={
+     (require == msg.sender[balance];
+     (require == 
+
+
+}
