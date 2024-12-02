0accfb5f0ec3d4bf4f6259da974c5e19bfd513ef
cj03nes <72360646+cj03nes@users.noreply.github.com>
2024-12-02 12:43:07 -0600

    Update EmployeePiPay.sol

--- contracts/EmployeePiPay.sol
+++ contracts/EmployeePiPay.sol
@@ -17,7 +17,8 @@ function createCompanyAccount ={
 company name = $companyName;
 first name = $firstName;
 last name = $lastName;
-headquarters address = $hqAddress;    }
+headquarters address = $hqAddress; 
+return companyAccount;}
 
 // create worker account
 function createWorkerAccount ={
@@ -26,11 +27,13 @@ last name = $lastName;
 address = $address;
 bank details = $bankingDetails;
 pi wallet address = $piAddress;
+return workerAccount;
 
 }
 
 // choose worker base pay
 const workerBasePay = minimumWage || salary || custom ;
+return worker[basePay];
 
 
 // choose worker timeIn/ timeOut
@@ -42,10 +45,12 @@ if time-in = 8am,
 then time-out = 6pm,
 return 6pm - 8am = 10 hours;
 hoursWorkedInADay = 10 hours;
+return hoursWorkedInADay;
 }
 
 // connect worker wallet[address]
 connectWallet || copy & paste manually;
+return worker[wallet.address];
 
 
 
@@ -58,9 +63,11 @@ hoursWorkedInAPayPeriod * worker[basePay] = worker[paycheckBalanceInUSD];
 if hoursWorkedInAPayPeriod = 60 hours,
 && if worker[basePay] = $15,
 then return 60 hoursWorkedInAPayPeriod * $15 worker[basePay] = $900 InUSD;
+return worker[paycheckBalanceInUSD];
 
 await worker[paycheckBalanceInUSD] then return function buyPi;
 await function buyPi then transfer paycheckBalanceInPi to worker[wallet.address];
+return worker[paycheckBalanceInPi];
 
 
 }
