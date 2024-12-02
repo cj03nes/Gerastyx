27c6af3703f43cc1cca648db6b8da53b5bfc855d
cj03nes <72360646+cj03nes@users.noreply.github.com>
2024-12-01 19:39:02 -0600

    Update contracts

---
+++ contracts/EmployeePiPay.sol
@@ -0,0 +1,8 @@
+// create company account
+
+// create worker account
+
+// choose worker base pay
+// choose worker timeIn/ timeOut
+// connect worker wallet[address]
+// pay worker pi [USD denomination]
