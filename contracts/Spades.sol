3e997ed8ac7b4b22dfeb27ce7e5b6433cc0627a1
cj03nes <72360646+cj03nes@users.noreply.github.com>
2024-12-02 11:31:52 -0600

    Update Spades.sol

--- contracts/Spades.sol
+++ contracts/Spades.sol
@@ -1,1 +1,50 @@
-// P2P Spades for $PI
+pragma solidity ^0.8;
+
+// twoP vs twoP Spades for $PI
+
+
+contact Spades ={
+
+const cards;
+54 cards = { 
+Ace = 4( 1 heart, 1 club, 1 diamond, 1 spade);
+Two = 4( 1 heart, 1 club, 1 diamond, 1 spade);
+Three = 4( 1 heart, 1 club, 1 diamond, 1 spade);
+Four = 4( 1 heart, 1 club, 1 diamond, 1 spade);
+Five = 4( 1 heart, 1 club, 1 diamond, 1 spade);
+Six = 4( 1 heart, 1 club, 1 diamond, 1 spade);
+Seven = 4( 1 heart, 1 club, 1 diamond, 1 spade);
+Eight = 4( 1 heart, 1 club, 1 diamond, 1 spade);
+Nine = 4( 1 heart, 1 club, 1 diamond, 1 spade);
+Ten = 4( 1 heart, 1 club, 1 diamond, 1 spade);
+Jack = 4( 1 heart, 1 club, 1 diamond, 1 spade);
+Queen = 4( 1 heart, 1 club, 1 diamond, 1 spade);
+King = 4( 1 heart, 1 club, 1 diamond, 1 spade);
+Joker = 2( 1 small, 1 big);   }
+
+red = heart && diamond;
+black = club && spade;
+
+
+// start game / deal deck
+// choose # of boards && make bet for $
+// play game / rules
+// game to 30 boards
+// game over / winner
+// pay winners
+
+function deal ={
+
+msg.sender[Player1] + msg.sender[Player2]  vs msg.sender[Player3] + msg.sender[Player4];
+
+}
+
+function chooseBoards ={}
+function chooseWager ={}
+
+function gameRules ={}
+
+function whoWOn ={}
+function payWinners ={}
+
+}
