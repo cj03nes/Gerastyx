0db68cdc4637a338c0a3969ac4ba99b2443c9120
cj03nes <72360646+cj03nes@users.noreply.github.com>
2024-12-02 12:40:16 -0600

    Update JobFinderMatch.sol

--- contracts/JobFinderMatch.sol
+++ contracts/JobFinderMatch.sol
@@ -1,9 +1,50 @@
 pragma solidity ^0.8;
 
-// create user account
+// create company account, kyc/aml, bbb
 
-// create company account
+function createCompanyAccount ={
+company name = $companyName;
+first name = $firstName;
+last name = $lastName;
+headquarters address = $hqAddress; 
+return companyAccount;}
+
+// create worker account
+function createWorkerAccount ={
+first name = $firstName;
+last name = $lastName;
+address = $address;
+bank details = $bankingDetails;
+pi wallet address = $piAddress;
+return WorkerAccount;
+}
 
 // list jobs
+function jobsHiring ={
+company name = $companyName;
+company job title = $jobTitle;
+company job description = $jobDescription;
+company job salary = $jobSalary;
+company job locaton = $jobLocation;
+company job contact information = $jobContactInfo;
+
+}
+
+
+
 // apply for jobs
+function applyForJob ={
+first name = $firstName;
+last name = $lastName;
+job experience = $jobExperience;
+location = $locaton;
+contact information = $contactInformation;
+
+}
+
+
 // message back-forth
+function jobBoardChat ={
+
+
+}
