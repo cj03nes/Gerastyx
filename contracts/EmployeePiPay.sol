841d8b28ef55a0b645678f7c28e025736dd7b635
cj03nes <72360646+cj03nes@users.noreply.github.com>
2024-12-02 12:32:53 -0600

    Update EmployeePiPay.sol

--- contracts/EmployeePiPay.sol
+++ contracts/EmployeePiPay.sol
@@ -1,8 +1,66 @@
-// create company account
+pragma solidity ^0.8;
+
+// import InstilledInteroperability;
+// import buyPi function;
+// import KYC/AML so too many accounts arent nested
+
+
+
+// ADP, WORKFORCE, ETC
+
+contract EmployeePiPay ={
+
+
+// create company account, kyc/aml, bbb
+
+function createCompanyAccount ={
+company name = $companyName;
+first name = $firstName;
+last name = $lastName;
+headquarters address = $hqAddress;    }
 
 // create worker account
+function createWorkerAccount ={
+first name = $firstName;
+last name = $lastName;
+address = $address;
+bank details = $bankingDetails;
+pi wallet address = $piAddress;
+
+}
 
 // choose worker base pay
+const workerBasePay = minimumWage || salary || custom ;
+
+
 // choose worker timeIn/ timeOut
+const workerTimeLog ={ 
+console.log(time-in);
+console.log(time-out);
+time-out - time-in = hoursWorkedInADay;
+if time-in = 8am,
+then time-out = 6pm,
+return 6pm - 8am = 10 hours;
+hoursWorkedInADay = 10 hours;
+}
+
 // connect worker wallet[address]
+connectWallet || copy & paste manually;
+
+
+
 // pay worker pi [USD denomination]
+     (require == workerTimeLog);
+if payPeriod = every 2 weeks,
+then the sum of hoursWorkedInADay,
+return hoursWorkedInAPayPeriod;
+hoursWorkedInAPayPeriod * worker[basePay] = worker[paycheckBalanceInUSD];
+if hoursWorkedInAPayPeriod = 60 hours,
+&& if worker[basePay] = $15,
+then return 60 hoursWorkedInAPayPeriod * $15 worker[basePay] = $900 InUSD;
+
+await worker[paycheckBalanceInUSD] then return function buyPi;
+await function buyPi then transfer paycheckBalanceInPi to worker[wallet.address];
+
+
+}
