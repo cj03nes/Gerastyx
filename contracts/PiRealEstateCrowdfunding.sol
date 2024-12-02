a7f12262d2fbf18459725002e5e710f2a7f12e91
cj03nes <72360646+cj03nes@users.noreply.github.com>
2024-12-02 14:05:54 -0600

    Update PiRealEstateCrowdfunding.sol

--- contracts/PiRealEstateCrowdfunding.sol
+++ contracts/PiRealEstateCrowdfunding.sol
@@ -1,1 +1,27 @@
-// 
+pragma solidity ^0.8;
+
+
+
+
+
+
+// buy stadium, mall, city, etc
+// connect payment processor in venue
+import PiPayment Processor;
+
+// create stadiumNFT, mallNFT, cityNFT, or etc
+
+// allow people to invest in real-estate
+// pay NFT holders 5% of revenue made from payment processors in said venue
+
+contract realEstateCrowdfunding ={
+
+// * image or images here *
+// price = quantity % cost of land & development
+// quantity = < 100k 
+// 5% revenue to reCrowdfunding mktcap
+
+function buy ={}
+
+function sell ={}
+}
