289c5a12de371f3fae7f0e7e3ee8682e3c132795
cj03nes <72360646+cj03nes@users.noreply.github.com>
2024-12-12 13:35:19 -0600

    Create piPortion.sol

---
+++ contracts/piPortion.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.8;
+
+// index token
+
+// 1 $PI + 1 Gold Bullion + $1 USD = price of 1 $PiP
+
+// $PI pricefeeds / oracles
+// Gold Bullion pricefeeds / oracles
+// $1 USD = $1 USD, pricefeeds / oracles
+// import { verifiedTokenAssets} from instilledInteroperability
+
+// buy
+// sell
+// stake
+
+
+contract piPortion ={  
+
+// create index token
+
+
+
+function buy ={}
+
+function sell ={}
+
+function stake ={}
+
+}
