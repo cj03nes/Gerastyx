84f3e7e41c6f01895c7f154517849661fada9e22
cj03nes <72360646+cj03nes@users.noreply.github.com>
2024-12-12 13:33:34 -0600

    Create taxReturn.sol

---
+++ contracts/taxReturn.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.8;
+
+contract taxReturn.sol = {
+
+// file taxes through an aggregate of LICENSED tax services
+// h&r block
+// turbo tax
+
+
+function fileTaxes ={
+     (require == msg.sender[KYC];
+
+  
+}
+
+
+
+// receive taxReturn in $PI
+// $1 fee to core team revenue or $PI mktcap
+
+function taxReturn ={
+     
+
+
+}
+
+
+return W2Wages;
+
+}
