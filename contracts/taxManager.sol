a1309797b1e6a1e86e90fd9f0392a0cbb9135f22
cj03nes <72360646+cj03nes@users.noreply.github.com>
2024-12-12 13:34:24 -0600

    Create taxManager.sol

---
+++ contracts/taxManager.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.8;
+
+contract taxManager = {
+// mining
+const miningGains ={
+
+miningGainsInPi = { msg.sender $amountOfPiMined;}
+miningGainsInPIUSDDenomination ={ msg.sender $amountOfPiMinedInUSDDenomination; }
+}
+
+
+// buy / sell
+
+const buyAndSellStats ={
+
+const totalAmountOfPiBought = {}
+const totalAmountOfPiBoughtInUSDDenomination = {}
+
+
+const totalAmountOfPiSold = {}
+const totalAmountOfPiSoldInUSDDenomination = {}
+}
+
+
+// gains / losses
+
+const gainsAndLossesDeficit ={
+     (require == miningGains);
+     (require == buyAndSellStats);
+totalAmountOfPiSoldInUSDDenomination - totalAmountOfPiBoughtInUSDDenomination = buyAndSellDifference;
+if buyAndSellDifference > 1, then return gains($buyAndSellDifference),
+else if buyAndSellDifference < 0, then return losses($buyAndSellDifference);
+}
+
+
+return taxReport;
+
+}
