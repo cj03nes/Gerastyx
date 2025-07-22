4d09c0527226a311d56a135be32bc35b54729a05
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-07-22 10:12:45 -0500

    Create GateWayUtilityWaterBridge.sol

---
+++ contracts/GateWayUtilityWaterBridge.sol
@@ -0,0 +1,142 @@
+pragma solidity ^0.8;
+
+// physical digitized doorway
+// locationA => locationB
+// clean water to under-represented communities
+// no life forms pass through these dead or alive
+// 75% to coreteam, 25% to cj03nes
+import Zeropoint from "./Zeropoint.sol";
+import { connectDevice , deviceConnected, deviceInformation, accountBalances } from "./deviceConnect.sol" ; 
+owner.address = cj03nes;
+
+
+
+
+// chain to chain
+mapping(chainA) => mapping(chainB) => crossChain;
+
+// chain to device
+mapping(chain) => mapping(device => mapping(deviceInformation) ) => deviceConnected;
+
+//on-chain Zeropoint to on-chaindevice
+mapping(chain => mapping(uint256 Zeropoint) => mapping(chain => mapping(device => mapping(deviceInformation) ) ) => zeropointConsumedToDevice;
+
+//on-chain ZeropointWifi to on-chain device
+mapping(chain => mapping(uint256 ZeropointWifi) => mapping(chain => mapping(device => mapping(deviceInformation) ) ) => zeropointwifiConsumedToDevice;
+
+// on-chain zeropoint and on-chain zeropointwifi to on-chain device
+mapping(chain => mapping(uint256 Zeropoint) ) && mapping(chain => mapping(uint256 ZeropointWifi) ) => mapping(chain => mapping(device => mapping(deviceInformation) ) ) zeropointAndZeropointWifiConsumedToDevice;
+
+// on-chain zeropoint and zeropointwifi to device location to on-chain zeropoint and zeropointwifi to device location
+mapping(chain => mapping(uint256 Zeropoint) && mapping(chain => mapping(uint256 ZeropointWifi) => mapping(device => mapping(deviceInformation => mapping ( deviceLocation ))))) => mapping(chain => mapping(uint256 Zeropoint) && mapping(chain => mapping(uint256 ZeropointWifi) => mapping(device => mapping(deviceInformation => mapping ( deviceLocation ))))) => deviceLocationAToDeviceLocationB;
+
+
+
+contract gateWayUtilityWaterBridge ={
+
+// connect physical doorways
+function connectGateWay ={
+     (require == connectDevice);
+const connectDevice returns deviceConnected;
+
+deviceConnected = gateWayConnected; }
+
+
+// create a bridge between digital doorwayA to digital doorwayB
+function hereToThere = {
+     (require == deviceConnected && gateWayConnected);
+     (require == zeropointAndZeropointWifiConsumedToDevice);
+
+// get the location of digital doorway A
+const chooseLocationA ={
+     (require == deviceConnected && gateWayConnected);
+     (require == zeropointAndZeropointWifiConsumedToDevice);
+     (require == gateWayConnected(deviceInformation[ipAdress && PhoneWifiMACAddress]) );
+}
+// get the location of digital doorway B
+const chooseLocationB ={
+     (require == deviceConnected && gateWayConnected);
+     (require == zeropointAndZeropointWifiConsumedToDevice); 
+     (require == gateWayConnected(deviceInformation[ipAdress && PhoneWifiMACAddress]) );
+
+
+return deviceLocationAToDeviceLocationB; } }
+
+
+
+
+function buyWater ={
+     (require == verifiedChains);
+     (require == verifiedTokenAssets);
+     (require == msg.sender(balance);
+
+if msg.sender(balance) < utilityWaterBill then return error("Insufficient Funds"),
+else if msg.sender(balance) >= utilityWaterBill then return utilityWaterBillPaid;
+msg.sender(balance) - utilityWaterBillPaid = msg.sender(newBalance);
+owner.address(balance) + utilityWaterBillPaid = owner.address(newBalance);
+
+
+
+
+newYork ={
+utilityWaterBill =[
+newYorkToLondon = ( $10 USD ),
+newYorkToCalifornia = ( $10 USD ),
+newYorkToHawaii = ( $10 USD ),
+newYorkToDallas = ( $10 USD ),
+newYorkToNewOrleans = ( $10 USD ),
+newYorkToAtlanta = ( $10 USD ),
+newYorkToFlorida = ( $10 USD ); ]}
+
+
+dallas ={
+utilityWaterBill =[
+dallasToLondon = ( $10 USD ),
+dallasToCalifornia = ( $10 USD ),
+dallasToHawaii = ( $10 USD ),
+dallasToNewYork = ( $10 USD ),
+dallasToNewOrleans = ( $10 USD ),
+dallasToAtlanta = ( $10 USD ),
+dallasToFlorida = ( $10 USD ); ]}
+
+
+newOrleans ={
+utilityWaterBill =[
+newOrleansToLondon = ( $10 USD ),
+newOrleansToCalifornia = ( $10 USD ),
+newOrleansToHawaii = ( $10 USD ),
+newOrleansToNewYork = ( $10 USD ),
+newOrleansToDallas = ( $10 USD ),
+newOrleansToAtlanta = ( $10 USD ),
+newOrleansToFlorida = ( $10 USD ); ]}
+
+
+
+atlanta ={
+utilityWaterBill =[
+atlantaToLondon = ( $10 USD ),
+atlantaToCalifornia = ( $10 USD ),
+atlantaToHawaii = ( $10 USD ),
+atlantaToNewYork = ( $10 USD ),
+atlantaToDallas = ( $10 USD ),
+atlantaToNewOrleans = ( $10 USD ),
+atlantaToFlorida = ( $10 USD ); ]}
+
+
+florida ={
+utilityWaterBill =[
+floridaToLondon = ( $10 USD ),
+floridaToCalifornia = ( $10 USD ),
+floridaToHawaii = ( $10 USD ),
+floridaToNewYork = ( $10 USD ),
+floridaToDallas = ( $10 USD ),
+floridaToNewOrleans = ( $10 USD ),
+floridaToAtlanta = ( $10 USD ); ]}
+
+
+
+
+}
+
+
+}
