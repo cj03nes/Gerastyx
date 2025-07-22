ed4a4cf3ec36bf420b6f50023f8c4c49f4d68799
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-07-22 10:14:12 -0500

    Create ZeropointShield.sol

---
+++ contracts/ZeropointShield.sol
@@ -0,0 +1,38 @@
+
+pragma solidity ^0.8;
+
+
+import Zeropoint from "./zeropoint.sol";
+// zeropoint + color, opacity, strength, etc
+// place between highway medians & exits to create anti-crash speed lanes
+// 
+
+// anti-collision & anti-terrorism
+
+contract ZeropointShield ={
+
+opacity: 0.5;
+position: absolute solid;
+color: #739BD0;
+outline: thick double #32a1ce;
+visibility: visible;
+
+
+async function createForcefield ={
+     (require == zeropoint);
+     (require == zeropointWifi);
+encapsulate powerBox{
+powerBox = zeropoint(from);  
+encapsulate = surround(); } }
+
+if xCar = collision, then return reboundCushion,
+else if xCar = !collision then return absolute solid;
+
+reboundCushion = absolute solid + catch(object);
+
+console.log(ZeropointShield = absolute solid reboundCushion catch);
+
+
+
+}
+
