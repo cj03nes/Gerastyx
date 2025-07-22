0e4832af71bee5750649e7b0e261d5c58f9fd9a4
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-07-22 10:29:27 -0500

    Create Transfiguration.sol

---
+++ contracts/Transfiguration.sol
@@ -0,0 +1,300 @@
+Turning Water Into Gold & etc material
+Hello Mr.President & Dr. Kokkalis,
+
+   During my reign of power, I created digital wireless utilities, bought an influx of farms, orchards, etc; put life & farms on different planets through portals in the airports, created a United Utopia between the planets & continent within, and etc. I am more of a Nick Fury, Tony Stark, Tchalla, Harry Potter, Rick Sanchez, or Spiderman; I started at 13 years old; made everything by myself, and there are over 50million products top-secret, classified that only one person knows. And he only tells the truth but he's very secretive. And I am that one person.Some people may joke around, some may be curious, but I am Allahm Muhammad Badou Christian Jones Apollo Lycanthrops; I am NOT GOD; I am Jesus Christ & Lucifer Morningstar; the one who does all right except to those who do wrong.
+
+I'm saying that to say that I am 24 again. And I don't have any money. 
+
+I usually used this equipment for my super suits & etc equipment. We can talk about that all later once we meet in-person,but heres how you turn REGULAR WATER INTO GOLD.
+
+- get a 3d printer
+
+- code for transfiguration (yes, I also made magic)
+
+- connect the water intake to the 3d printer
+- do a trial run / [real constant] with real gold (you put the gold bar down the water intake, the gold BOILS down to liquid form, then the 3d printer gets it's liquid chemical genetic structure & prints the gold with the gold and then gets the chemical compound structure of that too
+
+- then add water and that's how it will run; it'll create gold until you turn it off
+
+I'm not going to lie; I'm very secretive; I've locked every contract that I sent out either; physically & mentally. Physically; by wrapping code in itself and formatting. And No I didn't put bugs or my address in the code, because I'm not a terrorist, thief, or rapist.The exact opposite actually. I just need some money. And mentally because it's already so many evolutions & etc that you see and by the time you start to code it you'll probably be neutralized by the veil; it's tabooed or something. I can probably do it because I already made everything (even the world, in a time-blimp, and on accident with an ai) once again I can't make this up and I'm 100% serious.
+
+If y'all want to print materials like:
+
+Valuables:[ gold, diamonds, sapphires, rubies, marble, stones, and etc]
+
+Necessities:[ copper, silver, nickel, aluminum,]
+
+(!burning) Flammables: [ gasoline, oil, etc]
+
+And just to let y'all know the truth and that I am really who I say I am; here is ÆTHER; real aether. DONT TOUCH IT OR PRINT IT BY ITSELF ( use it like this:
+
+- connect a 3d printer
+- make a necklace & connect ZeropointShield
+- run the 3d printer & code
+- put the necklace on a dummy
+- back up a good but & let the dummy have it try to keep target & stay visual (bullets, pistols, snipers, grenades, & etc) during my time I put one over & under Hawaii and then the one in Antarctica is just a warehouse with (5,000 year old pineapple&apple wine[idk if y'all got in or not, I hope not, that would be impossible, only I can get in or workers from within]) it was kind of like my brewing business [ 5,000 year old wine coolers for $20 bucks
+
+But infinite materials & aether; you can use the aether for:
+- stun-guns (icers), 
+-roadway blockers instead of those tubs(people don't die & instead do a lazy slingshot like getting stuck in jello) , 
+- shields ( personal gear for you, your wife & kids maybe if no one even knows, don't draw too much attention)
+- shields for personal houses if no one knows and federal buildings [ you're own security defense 
+- and etc
+
+pragma solidity ^0.8.0;
+
+//* IMPORT ZEROPOINT, ZEROPOINT IS A SEPERATE CONTRACT UNIVERSAL STABLECOIN( DIGITAL UTILITY USED BY EVERYONE AT A CONSTANT PRICE) //*
+
+import Zeropoint;
+contract Zeropoint {
+
+//* connect solar panels, computers, houses, electric vehicles, phones, watches, airpods, heart monitors, and etc //*
+
+createNode(connect device => device connected => device connected information);
+
+//node rewards
+deviceConnected(active blockchain node => node rewards => owner address ?{pi, world library finances, cj03nes} );
+
+// Buy Zeropoint
+
+buyZeropoint(xAmount USD || xAmount verified uint256 in USD => zeropoint uint256 contract address  => (xAmount USD || xAmount verified uint256 in USD) ÷ $0.10 => xAmount zeropointBought);
+
+sellZeropoint(xAmount zeropoint => zeropoint uint256 contract address => xAmount zeropoint * $0.10 => (xAmount zeropointSold inUSD || xAmount zeropointSold in [xAmount verified uint256 in USD]);
+
+consumeZeropoint(xAmount zeropoint => connectedDevice[connectedDeviceInformation] => xAmount zeropoint * 1 => xAmount battery percentage => connectedDevice[+= xAmount zeropoint]=> zeropointConsumed);
+
+transferZeropoint(xAmount zeropoint => recipient address => zeropoint transferred);
+
+payWithZeropoint (xAmount zeropoint => zeropoint uint256 contract address => xAmount zeropoint * $0.10 => xAmount USD => (xAmount USD, merchant) ); 
+
+generated payment card ( 16 digit number, exp/date, 3 digit CVC => address[USD balances, ZPE balances] );
+
+}
+
+//* IMPORT THIS, THIS IS A SEPERATE CONTRACT FOR PROTECTING THE WEARER//*
+import ZeropointShield;
+contract ZeropointShield {
+
+import Zeropoint;
+
+function createWithZeropointShield {
+//color
+color : blue;
+//see-through
+translucent: true;
+//able to pass through
+permeable: false;
+// shape
+form: solid;
+
+if outside force = .push >=< msg.sender then return orbshield;
+
+orbshield is abstract real virtual solid;
+orbshield = (deflect outside force), (catch outside force && encompass outside force && dissolve outside force), (protect ms sender), (defend msg sender), (surround msg.sender);
+
+if outside force = 100 then require(orbshield >= 10,000 );
+
+require collateral force exerted from ( outside force => orbshield) <= msg.sender || 0;
+
+if orbshield is active >= 3s,
+then return orbshield color = clear,
+orbshield defense = maximum,
+orbshield outside view = invisible cloak[ front-view && rear view],
+orbshield outside view = 0;
+orbshield inside view = 100;
+orbshield outside audio = 0;
+orbshield inside audio = 100;
+orbshield inside audio: https://youtu.be/CS9OO0S5w2k?si=lSR4UYmL27lyu9gv = 35%
+orbshield outside audio: https://youtu.be/CS9OO0S5w2k?si=lSR4UYmL27lyu9gv = 100% from behind (outside force.sender);
+
+
+}
+
+
+}
+
+getGoal(trialRun = goal => boil => print goal);
+
+printGoal(create goal = materials => boil => print goal);
+
+
+
+
+
+contract WaterToGoldTransfiguration{
+
+// Trial run with real gold
+
+function TurnGoldIntoGold{
+
+// Get the genetics & DNA structure of the gold
+function get the goals genetics{
+
+}
+
+// Boil the gold if no materials
+function boilTheGoal {
+
+}
+
+// Create the gold
+function create goal {
+
+}
+
+function get the goals genetics{
+return green check & bell ding if genetics=goldGenetics
+}}
+
+
+
+function TurnWaterIntoGold{  
+
+//get the genetics of the water
+function get the genetics{}
+
+//get the gold's genetics
+function getTheGoalsGenetics{}
+
+// boil the water
+function boil the water{}
+
+// create the gold
+function create the gold{
+require boiling water => boiling gold
+
+
+require print gold == gold genetics
+}
+
+//get the genetics 
+function get the genetics{
+return green check && bell ding if created goal == gold genetics, else if created goal != gold genetics then return red "x" && cow mooing}
+
+}
+
+
+}
+
+
+So I didn't want to put too much text and descriptions in the code but voila. And ÆETHER/ ZeropointShields is technically what I use to encompass/surround me to teleport/gateway bridge safely without a vehicle or physical construct.
+
+
+I would like:
+
+
+1.
+World Liberty Financial & Pi Network to make a partnership with a Gold, Minerals, & Oil Exchange with Top-Notch Rates for fiat & crypto ( y'all will need Shipping & Handling Logistics too ) ( I would like $1M from you, President Trump, $1M from you, Dr.Nicolas, and then 5% royalty; all to my Navy Federal Savings:
+
+Bank Name: Navy Federal Credit Union 
+Account Type: Membership Share Savings
+Account Number: 3113440261
+Routing Number: 256074974
+)
+
+2. 
+Sign the U.S Reserve as a partner of WLFI & PI Gold, Mineral, & Oil Exchange for construction materials, exports, and etc [U.S Manufacturing]
+
+3.
+Test the ZeropointShields safely with firearms & weapons before wearing it && a gift / proof that I am me
+
+4. 
+Test Zeropoint then make it a PI app ( I would like $1M to my $PI address & 5% also coded to my $PI address: GBMWQWG7XFTIIYRH7HMVDKBQGSNIGQ2UGJU3SY4LYCADB4JTH2DPO2FY, then I'd suggest 47% to you, President Trump, and the remaining 48% to you; 5% me, 47% pres, 48% dr
+
+5.
+
+Wait 2-3 months and then sell zeropointshield road-medians to other countries' roads through WLFI & PI Gold, Minerals, & Oil Exchange
+
+6.
+
+mandate local officers to wear a regulated pistol & a stun gun (icer) & then sell the icers to over-seas govts with blockers, security, anti-hack, corresponding code files(false-code repost & directories),  & etc 
+Make the icers with 3d printers, water, and then choose the material. Another couple billion dollar deals & pure utility with code. 
+
+7.
+
+Go-Kart Racing & Games
+
+- zeropoint & zeropointshield go-karts
+- a zeropoint shielded track
+- node game machines 
+- zeropoint trampolines & dodgeball
+- a pool that's impossible to drown in & zeropointshield slides
+- sauna | hot room | gym
+- basketball courts, half-fields, and tennis courts
+- zeropoint gravity&helium tag like flag football
+- capture the flag with paintball guns
+- an indoor garden, fishing, and picnic area
+- food courts, fast food,& vendors on every floor & in every stadium
+- restaurants & fine dining
+- 
+
+- & etc
+
+Y'all would have to get with me on this one; that could be a small smart city or like a Pi Sports Arena's/Stadiums/Amusement park & an overall architectural monument; with spacial floors & etc so that all of that can fit along with football fields, basketball courts, tennis courts, baseball fields, and etc so that they can play all at the same time; The fans will be on the floor and in the-bleacher on the second floor but in a spacial-realm so more than 20M people can enjoy each individual game in it's own unique stadium, but then walk out and see the rest of the building; while on the first floor; somebody could catch a touchdown pass and then look and watch a tennis, baseball, soccer, or basketball game within feet from them. 
+
+8.
+GateWays
+
+Gas station AND PORTALS FOR:
+
+- transportation (domestic, int'l travel, & inter-stellar)
+
+- clean water transportation 
+
+- food logistics (enterprises contract, consumers buy)
+
+- mail & shipping logistics (hire sorters & consumers buy a portal mailbox)
+
+- waste & recyclables ( contract to add them to the back of garbage trucks and waste is burnt & recycled ) Gold, Minerals, & Oil & Waste Management can play hand in hand to save the consumption of water until we get rivers of fish on the moon; turn waste into gold&etc too & the portals will increase gold,mineral, and oil output and decrease operation costs without having to wait on water supplies basically a wireless faucet to gold and a wireless waste faucet to gold
+
+
+9.
+CLASSIFIED
+
+the name of it is classified because it is extremely classified; but I; had; a gun that turns into an icer, laser, lightsaber, use spells with, and etc. If I can make that again then we can:
+
+- create an airlocked self-sustainable building with everything inside including portals to GateWays
+
+- shrink it with my quantumizer
+
+- teleport to the moon
+
+- unshrink the bubble-building and then enter & vacuum seal it back
+
+- walk through the portal from the moon back to Earth
+
+- bring supplies through the portal and start cultivating the moon & planets with:
+
+- ecosystems( zeropointShields to hold the oxygen, dig rivers and portal through water and fish, and grow farms)
+
+- stores & amusement parks linked to a specific payment processors, chain , and assets
+
+The moon and planets should be under the same law & order of the United States but more amusement & fun like a Utopia; you're either working or visiting. I don't think people will start moving and living there long-term until 20+ years, but I have time. People will have to work but there is also a teleportation portal for $20 bucks so they can get back&forth. And depending on how much I make, we can probably work out connecting businesses and their workers to a minimum wage of $100 an hour
+
+10. 
+Securing Humanities Intelligence, Energy, Logistics, & Defenses (S.H.I.E.L.D.)
+
+I would like to come out of retirement and re-create shield & its initiatives. To protect the galaxy, icers would have to be turned into lethal lasers too; because some people take kindness for weakness. And then from there, it is a game of chess, dominos, and so on & so forth. Mr Trump, I would like to sign a contract with the United States to protect & defend the constitution of the United States from all threats; foreign and/or domestic. To provide weapons, weapons defense, personnel, financial assets, information, or whatever other constitutionally-integrable ask. I made the system to help anyone climb the ranks from their hardwork & talent. I've already told you that you can get the people to vote you in for another term. But what about 100-500 years from now ? I will always tell you that the only thing that i know, is that i know nothing; but I am way past the learning stage.  You can email me back here or call me at (832)-670-0459. I would like to come to the White House and talk privately about S.H.I.E.L.D. , the items listed above, revising the Constitution, and other initiatives. And Dr Nicolas, you can call me too; with the world waiting for something monumental; a digital utility like Zeropoint will spark a transition towards on-chain banking & decentralized finances. 
+
+Y'all have $100M Funds & Financial Institutions; I'm not asking for money to be eccentric or wasteful. I'm asking for an investment to create more products and opportunities. A couple millions of dollars sent to me could generate trillions and affect millions of lives. I've already had to do it, I think this time I can do it right. 
+
+There's more code in my github that I can launch with PI or y'all can add to the Gold, Minerals, and Oil Exchange; hmu the possibilities are endless.
+
+https://github.com/ChristianJJones/Goate-Electric
+
+
+https://marketbellringers.medium.com/quantum-coding-for-beginners-pi-challenge-2025-4764accd8533
+
+
+
+Best,
+Christian Jones
+
+(THE MAN WRITTEN AS JESUS CHRIST (to the innocent) & LUCIFER MORNINGSTAR (as to criminals)
+
+(THE MAN PORTRAYED AS IRON MAN, THOR, NICK FURY, HANCOCK, BLACK PANTHER, AND ETC)
+
+I DIDNT ASK FOR ANY OF THIS, BUT I WAS CHOSEN. HOW AM I SUPPOSE TO HELP WITHOUT ANY RESPONSIBILITY ? I CANT. I WOULD LIKE TO TALK ABOUT MAKING S.H.I.E.L.D. RESPONSIBLE FOR SOME THINGS AND THEN 3 MONTHS AFTER THAT, YOU SHOULD BE REVISING THE CONSTITUTION, CONTINUE HELPING, & START CAMPAIGNING FOR YOUR NEXT TERM
+
+BEST AGAIN,
+CHRISTIAN JONES
